faf24b44d4664793e1a4922783c29aef8cd3a1a4
Navi sureka <navisureka23@gmail.com>
2023-04-07 15:28:21 +0530

    added class-validator

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {CreateMessageDto} from "./dtos/create-message.dto";
+
 
 @Controller('messages')
-export class MessagesController {
-
-}
+export class MessagesController{
+    @Get('/')
+    listMessages(){
+       return "";
+    }
+    @Post("/")
+    createMessage(@Body() body: CreateMessageDto){
+      return ""
+    }
+   }
+   
