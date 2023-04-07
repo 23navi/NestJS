27bc4688118d804ceee39a5cc4116ef4cfd5bb74
Navi sureka <navisureka23@gmail.com>
2023-04-07 18:19:30 +0530

    to change branch

--- SG/messages-demo/src/messages/messages.repository.ts
+++ SG/messages-demo/src/messages/messages.repository.ts
@@ -1,12 +1,20 @@
 import { writeFile,readFile } from "fs/promises";
+
 export class MessagesRepository{
     async findAll(){
+        const content= await readFile("./../../messages.json","utf8");
+        const messages= JSON.parse(content);
+        return messages;
 
     }
     async findOne(id:string){
+        const content= await readFile("./../../messages.json","utf8");
+        const messages= JSON.parse(content);
+        return messages[id];
 
     }
     async create(message:string){
+        
         
     }
 }
