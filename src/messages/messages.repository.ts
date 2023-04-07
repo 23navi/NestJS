0c4d71dc9ade13c44d0cb34aab684e71a5593b68
Navi sureka <navisureka23@gmail.com>
2023-04-07 18:01:08 +0530

    message repository start

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,12 @@
+import { writeFile,readFile } from "fs/promises";
+export class MessagesRepository{
+    async findAll(){
+
+    }
+    async findOne(id:string){
+
+    }
+    async create(message:string){
+        
+    }
+}
