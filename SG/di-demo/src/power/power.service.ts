e02479cd9999ae1c7a8438bd8dd91ee89f7d1fb1
Navi sureka <navisureka23@gmail.com>
2023-04-08 15:58:48 +0530

    power value

--- SG/di-demo/src/power/power.service.ts
+++ SG/di-demo/src/power/power.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PowerService {
     constructor(){}
-
-    
+    supplyPower(power:number){
+        console.log("Supplying "+power);
+    }
 }
