b4344d3306380c797e15c90aa2a358c84d39b301
Navi sureka <navisureka23@gmail.com>
2023-04-08 16:16:50 +0530

    done project

--- SG/di-demo/src/cpu/cpu.service.ts
+++ SG/di-demo/src/cpu/cpu.service.ts
@@ -4,5 +4,9 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
     constructor(private powerService:PowerService){}
-    
+    compute(a:number,b:number){
+        console.log("Drawing 5 unit of power for computation")
+        this.powerService.supplyPower(5);
+        return a+b
+    }
 }
--- SG/di-demo/src/disk/disk.service.ts
+++ SG/di-demo/src/disk/disk.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class DiskService {
     constructor(private powerService: PowerService){}
+    readData(){
+        console.log("Drawing 10 unit of power for reading from the disk")
+        this.powerService.supplyPower(10);
+        console.log("Reading data from the disk...")
+        return "This is your data";
+    }
 }
