059ebc6c57426e6718e3015bf0ecebf5faae8733
Navi sureka <navisureka23@gmail.com>
2023-04-13 19:29:31 +0530

    added remove and update services

--- SG/car-value-project/src/users/users.service.ts
+++ SG/car-value-project/src/users/users.service.ts
@@ -17,6 +17,17 @@ export class UsersService {
     find(email:string){
         return this.repo.find({where:{email}})
     };
-    update(){};
-    remove(){};
+    async update(id:number,attrs:Partial<User>){
+        const user= await this.findOne(id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        Object.assign(user,attrs);
+        return this.repo.save(user);
+    };
+    async remove(id:number){
+        const user = await this.findOne(id);
+        if(!user){throw new Error("No user found")}
+        return this.repo.remove(user);
+    };
 }
