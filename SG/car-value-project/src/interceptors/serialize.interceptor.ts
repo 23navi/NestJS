61ef07b70fffda08e7271a4f9a50453fb21fc214
Navi sureka <navisureka23@gmail.com>
2023-04-25 22:24:39 +0530

    custom intercepter

--- SG/car-value-project/src/interceptors/serialize.interceptor.ts
+++ SG/car-value-project/src/interceptors/serialize.interceptor.ts
@@ -1,19 +1,23 @@
 import { UseInterceptors,NestInterceptor,ExecutionContext,CallHandler } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { classToPlain } from "class-transformer";
+import { plainToClass } from "class-transformer";
+import {UserDto} from "../users/dtos/user.dto";
 
 export class SerializeInterceptor implements NestInterceptor{
     intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        //run before the handler
-        console.log("Running before the handler");
-        console.log(context);
+        // //run before the handler
+        // console.log("Running before the handler");
+        // console.log(context);
 
         return handler.handle().pipe(
             map((data:any)=>{
-                //run after the req handler
-                console.log("Running after the handler");
-                console.log(data);
+                return plainToClass(UserDto,data,{
+                    excludeExtraneousValues:true,
+                })
+                // //run after the req handler
+                // console.log("Running after the handler");
+                // console.log(data);
             })
         )
     }
