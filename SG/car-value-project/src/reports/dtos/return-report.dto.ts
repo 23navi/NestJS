a8ebd3a864c307dbd89938aa348031c0462d2456
23navi <navisureka23@gmail.com>
2023-05-26 14:01:14 +0530

    return report dto

---
+++ SG/car-value-project/src/reports/dtos/return-report.dto.ts
@@ -0,0 +1,21 @@
+import { Transform, Expose } from 'class-transformer';
+export class ReturnReportDto {
+  @Expose()
+  price: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
+  year: number;
+  @Expose()
+  lng: number;
+  @Expose()
+  lat: number;
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
--- SG/car-value-project/src/users/decorators/current-user.decorator.ts
+++ SG/car-value-project/src/users/decorators/current-user.decorator.ts
@@ -1,8 +1,10 @@
-import {
-    createParamDecorator,
-    ExecutionContext,
-} from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const CurrentUser = createParamDecorator((data:any, context:ExecutionContext)=>{
-    return "Hello Navi";
-})
+export const CurrentUser = createParamDecorator(
+  (data: any, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    console.log(request.currentUser);
+
+    return request.currentUser;
+  },
+);
