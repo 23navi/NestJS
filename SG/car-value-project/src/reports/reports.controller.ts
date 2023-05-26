528b8e359be32561843778166c35c9bdd38a1f00
23navi <navisureka23@gmail.com>
2023-05-26 12:51:31 +0530

    new report

--- SG/car-value-project/src/reports/reports.controller.ts
+++ SG/car-value-project/src/reports/reports.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { AuthGuard } from '../guards/auth.guard';
+import { UseGuards } from '@nestjs/common';
+
+
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
+  // @Get()
+  // findAll() {
+  //   return this.reportsService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.reportsService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
+  //   return this.reportsService.update(+id, updateReportDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.reportsService.remove(+id);
+  // }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  create(@Body() createReportDto: CreateReportDto) {
+    return this.reportsService.create(createReportDto);
+  }
+
+  // @Get('export')
+  // export() {
+  //   return this.reportsService.export();
+  // }
+}
--- SG/car-value-project/src/reports/reports.service.ts
+++ SG/car-value-project/src/reports/reports.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(createReportDto: CreateReportDto) {
+    const report = this.repo.create(createReportDto);
+    return this.repo.save(report);
+  }
+}
