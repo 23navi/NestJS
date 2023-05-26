817dff87c292a86dbbd3e8887668c120f5c1a031
23navi <navisureka23@gmail.com>
2023-05-26 13:26:53 +0530

    added association and findAll

--- SG/car-value-project/src/reports/reports.controller.ts
+++ SG/car-value-project/src/reports/reports.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from '../guards/auth.guard';
 import { UseGuards } from '@nestjs/common';
-
-
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.reportsService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.reportsService.findAll();
+  }
 
   // @Get(':id')
   // findOne(@Param('id') id: string) {
@@ -32,8 +32,8 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  create(@Body() createReportDto: CreateReportDto) {
-    return this.reportsService.create(createReportDto);
+  create(@Body() createReport: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(createReport, user);
   }
 
   // @Get('export')
--- SG/car-value-project/src/reports/reports.service.ts
+++ SG/car-value-project/src/reports/reports.service.ts
@@ -3,13 +3,21 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(createReportDto: CreateReportDto) {
-    const report = this.repo.create(createReportDto);
+  create(createReport: CreateReportDto, user: User) {
+    const report = this.repo.create(createReport);
+    console.log('User: ' + user);
+    report.user = user;
+    console.log('Report: ' + report);
     return this.repo.save(report);
+  }
+
+  findAll() {
+    return this.repo.find();
   }
 }
